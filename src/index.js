7f429d998d9f7c5fc71a042abe87dbb62d84b5ad
Anna Moskalik <annie.moskalik@gmail.com>
2019-10-12 12:47:09 +0300

    add adj

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function zeros(expression) {
-  // your solution
-}
+  let array = expression.split ("*");
+  let n = array.length;
+  let zeros = 0;
+  let s = 1;
+
+  for (let i = 0; i <= n; i++)
+    if (array[i].endsWith('!!')) {
+      let num = array[i].slice(-6, -2);
+        if (num % 2 == 1) {
+            zeros = 0;
+            }
+          else if (num % 10 == 0) {
+            zeros += 1;
+            }
+      }
+    else for (s = 1; (n / (5 ** s)) >= 1; s++) {
+        let zero = Math.floor (n / (5 ** s));
+        zeros += zero;
+      }
+  return zeros;
+    }
